2ffcf0df8410a01196c167f56f9ade8b21655107
andreaalboni <125800750+andreaalboni@users.noreply.github.com>
2023-05-19 12:40:04 +0200

    Add files via upload

---
+++ GMMmt.py
@@ -0,0 +1,132 @@
+import math
+import time, cv2
+from tkinter import filedialog
+import numpy as np
+from sklearn.mixture import GaussianMixture
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+my_dpi = 96
+DIM_MT_IMG = [500, 500]
+COMPONENTS_NUM = 12
+
+def distmt(x, y, img_size, mt):
+    dist_x = (x*mt[0])/img_size[0]
+    dist_y = (y*mt[1])/img_size[1]
+    return [math.sqrt(dist_x**2+dist_y**2), dist_x, dist_y]
+
+# X, Y : meshgrid
+def multigauss_pdf(X, Y, means, covariances, weights):
+  # Flatten the meshgrid coordinates
+    points = np.column_stack([X.flatten(), Y.flatten()])
+
+    # Number of components in the mixture model
+    num_components = len(means)
+
+    # Initialize the probabilities
+    probabilities = np.zeros_like(X)
+
+    # Calculate the probability for each component
+    for i in range(num_components):
+        mean = means[i]
+        covariance = covariances[i]
+        weight = weights[i]
+
+        # Calculate the multivariate Gaussian probability
+        exponent = -0.5 * np.sum((points - mean) @ np.linalg.inv(covariance) * (points - mean), axis=1)
+        coefficient = 1 / np.sqrt((2 * np.pi) ** 2 * np.linalg.det(covariance))
+        component_prob = coefficient * np.exp(exponent)
+
+        # Add the component probability weighted by its weight
+        probabilities += weight * component_prob.reshape(X.shape)
+
+    return probabilities
+
+def main():
+    xp, yp = [], []
+    path = r"C:\Users\albon\Desktop\Dataset\DSR\val-masks\val"
+    file_types = [('Image', '*.jpg;*.png'), ('All files', '*')]
+    name = filedialog.askopenfilename(title='Select an image:', filetypes=file_types, initialdir=path)
+    image = cv2.imread(name, 0)
+    IMG_SIZE = image.shape
+    plt.figure(figsize=(IMG_SIZE[0]/my_dpi, IMG_SIZE[1]/my_dpi), dpi=my_dpi)
+    plt.title("Prediction")
+    plt.axis('off')
+    plt.imshow(image, cmap='gray')
+
+    for i in range(IMG_SIZE[0]):
+        for j in range(IMG_SIZE[1]):
+            if image[i,j] == 255: 
+                #xp.append(j)
+                #yp.append(IMG_SIZE[1]-i)
+                k,x,y = distmt(i, j, IMG_SIZE, DIM_MT_IMG)
+                xp.append(y)
+                yp.append(DIM_MT_IMG[0]-x)
+    
+    #plt.show()
+
+    GMModel = GaussianMixture(n_components=COMPONENTS_NUM, covariance_type='full', max_iter=1000)
+    GMModel.fit(np.column_stack((xp, yp)))
+    # calculate BIC
+    # bic = GMModel.bic(np.column_stack((xp, yp)))
+
+    # get means and covariances
+    means = GMModel.means_
+    covariances = GMModel.covariances_
+    mix = GMModel.weights_
+    print("Means: {}".format(means))
+    print("Coveriances: {}".format(covariances))
+    print("Mixture proportions: {}".format(mix))
+
+    # generate grid
+    # xgrid = np.linspace(0,image.shape[0])
+    # ygrid = np.linspace(0,image.shape[1])
+    # X, Y = np.meshgrid(xgrid, ygrid)
+        #plt.figure(figsize=(IMG_SIZE[0]/my_dpi, IMG_SIZE[1]/my_dpi), dpi=my_dpi)
+    plt.figure(figsize=(DIM_MT_IMG[0]/my_dpi, DIM_MT_IMG[1]/my_dpi), dpi=my_dpi)
+    plt.scatter(xp[:], yp[:], marker='.', color='k', s=1)
+    plt.title("Points inside ROI")
+    plt.xticks([])
+    plt.yticks([])
+    #plt.show()
+
+    # plot distribution
+        #X = np.arange(0, IMG_SIZE[0], 0.1)
+        #Y = np.arange(0, IMG_SIZE[1], 0.1)
+    X = np.arange(0, DIM_MT_IMG[0], 0.1)
+    Y = np.arange(0, DIM_MT_IMG[1], 0.1)
+    X, Y = np.meshgrid(X, Y)
+    Z = multigauss_pdf(X, Y, means, covariances, mix)
+
+    fig, ax = plt.subplots(1, 1, figsize=(12,4), subplot_kw={"projection": "3d"})
+
+    #3D surface plot
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+    ax.set_xlabel("X"); ax.set_ylabel("Y")
+    ax.set_zticks([])
+    ax.set_title("Probability density function")
+    ax.invert_xaxis()
+    # ax.view_init(60,60,0)
+
+    # 2D heatmap
+    fig2, ax2 = plt.subplots(1, 1)
+
+    z_min = np.min(Z); z_max = np.max(Z)
+    c = ax2.pcolormesh(X, Y, Z, cmap='YlOrRd', vmin=z_min, vmax=z_max)
+    ax2.set_title("heatmap")
+    # fig2.colorbar(c, ax=ax2)
+        #ax2.set_xlim([0, IMG_SIZE[0]]); ax2.set_ylabel([0, IMG_SIZE[1]])
+    ax2.set_xlim([0, DIM_MT_IMG[0]]); ax2.set_ylabel([0, DIM_MT_IMG[1]])
+    # plt.savefig("gmm.png")
+    # files.download("gmm.png")
+    plt.show()
+
+
+    # img = plt.imread()
+    # img.plot(xp, yp, 'ro')
+    # plt.imshow(img)
+
+
+
+main()
